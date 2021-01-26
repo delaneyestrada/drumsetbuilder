81dd0de9aea7732d96d8ac2cdca42b8f3e351402
Dillon Estrada <dillon.estrada55@gmail.com>
2021-01-26 10:55:12 -0600

    fix merge conflict

--- src/firebase.js
+++ src/firebase.js
@@ -1,43 +1,7 @@
 import firebase from "firebase";
 import firebaseConfig from "./firebaseConfig";
 
-<<<<<<< HEAD
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-analytics.js"></script>
-
-// <script>
-//   // Your web app's Firebase configuration
-//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//   var firebaseConfig = {
-//     apiKey: "***REMOVED***",
-//     authDomain: "drum-set-builder.firebaseapp.com",
-//     databaseURL: "https://drum-set-builder.firebaseio.com",
-//     projectId: "drum-set-builder",
-//     storageBucket: "drum-set-builder.appspot.com",
-//     messagingSenderId: "299035189486",
-//     appId: "1:299035189486:web:e55912f68ca311a45453ff",
-//     measurementId: "G-T3WB5VZBCN"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
-// </script>
-import firebase from "firebase"
-
-const firebaseConfig = {
-    apiKey: '***REMOVED***',
-    authDomain: 'drum-set-builder.firebaseapp.com',
-    databaseURL: 'https://drum-set-builder.firebaseio.com',
-    projectId: 'drum-set-builder',
-    storageBucket: 'drum-set-builder.appspot.com',
-    messagingSenderId: '299035189486',
-    appId: '1:299035189486:web:e55912f68ca311a45453ff'
-}
-firebase.initializeApp(firebaseConfig)
-=======
 firebase.initializeApp(firebaseConfig);
->>>>>>> move firebase configuration
 
 // utils
 const db = firebase.firestore();
